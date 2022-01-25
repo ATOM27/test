cd2ee742ec2cb5aee6db2f9a8291cce7d874ca1f
Eugene Mekhedov <eugene.mekhedov@gmail.com>
2022-01-25 15:46:39 +0200

    initial

--- test2/ContentView.swift
+++ test2/ContentView.swift
@@ -21,15 +21,6 @@ struct ContentView: View {
             Button("Anotehr button") {
                 print("")
             }
-            Text("And another feature")
-            Text("And another feature")
-            Text("And another feature")
-            Button("Anotehr button") {
-                print("")
-            }
-            Button("Anotehr button") {
-                print("")
-            }
         }
     }
 }
