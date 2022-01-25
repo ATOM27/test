be19bb3720cdd355afe5e5f87fb2ffc7b1a5f57f
Yevhen Mekhedov <yevhen.mekhedov@customertimes.com>
2022-01-25 12:45:09 +0200

    f

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Yevhen Mekhedov on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
