ee2e07bc8b6f83cc9b99e0e9b580c7ea2f24bfcb
Eugene Mekhedov <eugene.mekhedov@gmail.com>
2022-01-26 20:53:34 +0200

    po

--- test2/ContentView.swift
+++ test2/ContentView.swift
@@ -30,9 +30,6 @@ struct ContentView: View {
             Text("And another feature")
             Image(systemName: "star")
             Text("And another feature")
-            Button("Anotehr button") {
-                print("")
-            }
         }
     }
 }
